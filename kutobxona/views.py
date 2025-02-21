682019b55a8533227924a4f4972e9725a0147d0c
bekzodbek01 <bekzodbek5201@gmail.com>
2025-02-21 19:20:06 +0500

    1

--- kutobxona/views.py
+++ kutobxona/views.py
@@ -2,11 +2,9 @@ from rest_framework.generics import ListAPIView
 from .models import (Requirements, Editorial, Archive, Abstract, Literature, Sources)
 from .serializers import (RequirementsSerializer, EditorialSerializer, ArchiveSerializer, abstractSerializer,
                           LiteratureSerializer, SourcesSerializer)
-import re
+
 from django.db.models import Q
 from rest_framework.pagination import PageNumberPagination
-from django.contrib.postgres.search import SearchVector, SearchQuery
-from django.conf import settings
 
 
 class CustomPagination(PageNumberPagination):
@@ -39,133 +37,78 @@ class AbstractListCreateView(ListAPIView):
 
 
 class EditorialListCreateView(ListAPIView):
-    queryset = Editorial.objects.all()
     serializer_class = EditorialSerializer
     pagination_class = CustomPagination
 
     def get_queryset(self):
-        queryset = self.queryset.order_by('order')
-        search_query = self.request.GET.get('search', '').strip()
+        queryset = Editorial.objects.all().order_by('order')
+        search_query = self.request.GET.get('search', '')
 
-        if not search_query:
-            return queryset
-
-        # Maxsus belgilarni olib tashlash (@, #, -, va boshqalar)
-        search_query = re.sub(r'[^\w\s]', '', search_query)
-        search_words = search_query.split()
-
-        # PostgreSQL bo'lsa
-        if 'postgresql' in settings.DATABASES['default']['ENGINE']:
-            search_vector = SearchVector('title', 'title_uz', 'title_en')
-            search_query = SearchQuery(search_query)
-            queryset = queryset.annotate(search=search_vector).filter(search=search_query)
-        else:
-            # SQLite va boshqa bazalar uchun optimallashtirish
-            q_objects = Q()
-            for word in search_words:
-                q_objects |= Q(title__icontains=word)
-                q_objects |= Q(title_uz__icontains=word)
-                q_objects |= Q(title_en__icontains=word)
-            queryset = queryset.filter(q_objects)
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(title_uz__icontains=search_query) |
+                Q(title_en__icontains=search_query)
+            )
 
         return queryset
 
 
 class ArchiveListCreateView(ListAPIView):
-    queryset = Archive.objects.all()
     serializer_class = ArchiveSerializer
     pagination_class = CustomPagination
 
     def get_queryset(self):
-        queryset = self.queryset.order_by('order')
+        queryset = Archive.objects.all().order_by('order')
         search_query = self.request.GET.get('search', '').strip()
 
-        if not search_query:
-            return queryset
-
-        # Maxsus belgilarni olib tashlash
-        search_query = re.sub(r'[^\w\s]', '', search_query)
-        search_words = search_query.split()
-
-        # PostgreSQL uchun Full-Text Search
-        if 'postgresql' in settings.DATABASES['default']['ENGINE']:
-            search_vector = SearchVector('title_uz', 'title_en', 'year')
-            search_query = SearchQuery(search_query)
-            queryset = queryset.annotate(search=search_vector).filter(search=search_query)
-        else:
-            # SQLite uchun optimallashtirilgan qidiruv
+        if search_query:
+            search_words = search_query.split()  # So‘zlarni ajratish
             q_objects = Q()
+
             for word in search_words:
                 q_objects |= Q(title_uz__icontains=word)
                 q_objects |= Q(title_en__icontains=word)
-                q_objects |= Q(year__icontains=word)
+                q_objects |= Q(year__icontains=word)  # Year maydonini ham to‘liq qidirish
+                q_objects |= Q(title__icontains=word)
+
             queryset = queryset.filter(q_objects)
 
         return queryset
 
 
 class LiteratureListCreateView(ListAPIView):
-    queryset = Literature.objects.all()
     serializer_class = LiteratureSerializer
     pagination_class = CustomPagination
 
     def get_queryset(self):
-        queryset = self.queryset.order_by('order')
-        search_query = self.request.GET.get('search', '').strip()
+        queryset = Literature.objects.all().order_by('order')
+        search_query = self.request.GET.get('search', '')
 
-        if not search_query:
-            return queryset
-
-        # Maxsus belgilarni olib tashlash
-        search_query = re.sub(r'[^\w\s]', '', search_query)
-        search_words = search_query.split()
-
-        # Agar PostgreSQL bo‘lsa va index qo‘yilmagan bo‘lsa
-        if 'postgresql' in settings.DATABASES['default']['ENGINE']:
-            search_vector = SearchVector('title', 'title_uz', 'title_en')
-            search_query = SearchQuery(search_query)
-            queryset = queryset.annotate(search=search_vector).filter(search=search_query)
-        else:
-            # SQLite uchun optimallashtirilgan qidiruv
-            q_objects = Q()
-            for word in search_words:
-                q_objects |= Q(title__icontains=word)
-                q_objects |= Q(title_uz__icontains=word)
-                q_objects |= Q(title_en__icontains=word)
-            queryset = queryset.filter(q_objects)
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(title_uz__icontains=search_query) |
+                Q(title_en__icontains=search_query)
+            )
 
         return queryset
 
 
 class SourcesListCreateView(ListAPIView):
-    queryset = Sources.objects.all()
     serializer_class = SourcesSerializer
     pagination_class = CustomPagination
 
     def get_queryset(self):
-        queryset = self.queryset.order_by('order')
-        search_query = self.request.GET.get('search', '').strip()
+        queryset = Sources.objects.all().order_by('order')
+        search_query = self.request.GET.get('search', '')
 
-        if not search_query:
-            return queryset
-
-        # @, #, - va boshqa belgilarni olib tashlash
-        search_query = re.sub(r'[^\w\s]', '', search_query)
-        search_words = search_query.split()
-
-        # PostgreSQL bo'lsa
-        if 'postgresql' in settings.DATABASES['default']['ENGINE']:
-            search_vector = SearchVector('title', 'title_uz', 'title_en')
-            search_query = SearchQuery(search_query)
-            queryset = queryset.annotate(search=search_vector).filter(search=search_query)
-        else:
-            # SQLite va boshqa bazalar uchun optimallashtirish
-            q_objects = Q()
-            for word in search_words:
-                q_objects |= Q(title__icontains=word)
-                q_objects |= Q(title_uz__icontains=word)
-                q_objects |= Q(title_en__icontains=word)
-            queryset = queryset.filter(q_objects)
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(title_uz__icontains=search_query) |
+                Q(title_en__icontains=search_query)
+            )
 
         return queryset
 #
