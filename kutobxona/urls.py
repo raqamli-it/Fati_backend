7dfca6deed3bee629a568dfbeb244c8ad13320db
bekzodbek01 <bekzodbek5201@gmail.com>
2025-02-21 16:54:43 +0500

    1

--- kutobxona/urls.py
+++ kutobxona/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     path('talablar/', RequirementsListCreateView.as_view(), name='talablar-list'),
     path('arxivlar/', ArchiveListCreateView.as_view(), name='arxiv-list'),
     path('avtoreferat/', AbstractListCreateView.as_view(), name='tahrirchi-list'),
-
     path('manbalar/', SourcesListCreateView.as_view(), name='manbalar-list'),
     path('adabiyotlar/', LiteratureListCreateView.as_view(), name='adabiyot-list'),
     # path('archive_documents/list/', Archive_documentsListCreateView.as_view(), name='archive-list'),
