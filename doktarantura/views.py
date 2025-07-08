7faa60ed14b63ac0a413382c34787927db0702a0
bekzodbek01 <bekzodbek5201@gmail.com>
2025-07-08 13:26:41 +0500

    add

--- doktarantura/views.py
+++ doktarantura/views.py
@@ -5,7 +5,7 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 6
+    page_size = 15
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
